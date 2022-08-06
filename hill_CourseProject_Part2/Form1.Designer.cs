541851f07174582dc19c9cca9b34b495483e8929
ben-hill33 <benjamin.hill28@gmail.com>
2022-08-05 17:21:41 -0700

    Add project files.

---
+++ hill_CourseProject_Part2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace hill_CourseProject_Part2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.PrintPaychecksButton = new System.Windows.Forms.Button();
+            this.EmployeesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(12, 12);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(99, 53);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(117, 12);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(99, 53);
+            this.RemoveButton.TabIndex = 1;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.Location = new System.Drawing.Point(222, 12);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(99, 53);
+            this.DisplayButton.TabIndex = 2;
+            this.DisplayButton.Text = "Display";
+            this.DisplayButton.UseVisualStyleBackColor = true;
+            // 
+            // PrintPaychecksButton
+            // 
+            this.PrintPaychecksButton.Location = new System.Drawing.Point(327, 12);
+            this.PrintPaychecksButton.Name = "PrintPaychecksButton";
+            this.PrintPaychecksButton.Size = new System.Drawing.Size(180, 53);
+            this.PrintPaychecksButton.TabIndex = 3;
+            this.PrintPaychecksButton.Text = "Print Paychecks";
+            this.PrintPaychecksButton.UseVisualStyleBackColor = true;
+            // 
+            // EmployeesListBox
+            // 
+            this.EmployeesListBox.FormattingEnabled = true;
+            this.EmployeesListBox.ItemHeight = 25;
+            this.EmployeesListBox.Location = new System.Drawing.Point(12, 71);
+            this.EmployeesListBox.Name = "EmployeesListBox";
+            this.EmployeesListBox.Size = new System.Drawing.Size(744, 329);
+            this.EmployeesListBox.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EmployeesListBox);
+            this.Controls.Add(this.PrintPaychecksButton);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Name = "MainForm";
+            this.Text = "Payroll System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.Button PrintPaychecksButton;
+        private System.Windows.Forms.ListBox EmployeesListBox;
+    }
+}
+
---
+++ hill_CourseProject_Part2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hill_CourseProject_Part2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
