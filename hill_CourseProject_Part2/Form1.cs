15bde31c4ca44da6c8b15cca6c255c87e33f4272
ben-hill33 <benjamin.hill28@gmail.com>
2022-08-15 07:48:11 -0700

    Created Salary class that inherits Employee class.
    - Overrides ToString() from parent to include string formatting.

--- hill_CourseProject_Part2/Form1.cs
+++ hill_CourseProject_Part2/Form1.cs
@@ -13,6 +13,7 @@ namespace hill_CourseProject_Part2
 
         private void AddButton_Click(object sender, EventArgs e)
         {
+
             // Add item to employee listbox.
             InputForm frmInput = new InputForm();
 
---
+++ hill_CourseProject_Part2/Salary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hill_CourseProject_Part2
+{
+    class Salary : Employee
+    {
+        // attributes
+        private double annualSalary;
+
+        // constructors
+        public Salary() : base()
+        {
+            annualSalary = 0.0;
+        }
+
+        public Salary(string firstName, string lastName, string ssn, 
+            DateTime hireDate, Benefits benefits, double annualSalary) 
+            : base(firstName, lastName, ssn, hireDate, benefits)
+        {
+            AnnualSalary = annualSalary;
+        }
+
+        // behaviors
+        public override string ToString()
+        {
+            return base.ToString() + ", Salary: " + annualSalary.ToString("C");
+        }
+
+        // properties
+        public double AnnualSalary
+        {
+            get
+            {
+                return annualSalary;
+            }
+            set
+            {
+                if(value > 0.0 && value < 1000000.0)
+                {
+                    annualSalary = value;
+                }
+                else
+                {
+                    annualSalary = 0.0;
+                }
+            }
+        }
+    }
+}
